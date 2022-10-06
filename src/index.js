c765498142de48f95027e49c32e393a7fe34a1b7
Shuhei Shibahara <Shoehey06@gmail.com>
2022-10-06 10:17:38 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.getElementById('canvas')
+  const ctx = canvas.getContext("2d"); 
+  canvas.height = 400;
+  canvas.width = 800;
+});
+
