9e5947fd6775bc54d1fe02577d55548800f8ad42
Shuhei Shibahara <Shoehey06@gmail.com>
2022-10-07 12:21:09 -0700

    uploaded fish

--- src/script/fish.js
+++ src/script/fish.js
@@ -1,5 +1,4 @@
 import {Util} from "./util.js"
-import {Game} from "./game.js"
 
 class Fish {
   constructor (pos){
@@ -9,14 +8,18 @@ class Fish {
     // this.color = options.color;
     // this.game = Game();
   }
-
+  
   draw(ctx) {
-    ctx.fillStyle = 'orange';
-    ctx.beginPath();
-    ctx.arc(
-      this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true
-    );
-    ctx.fill();
+    const nemo = new Image();
+    nemo.src = 'src/image/clownFish.png'
+    // debugger
+    // ctx.fillStyle = 'orange';
+    // ctx.beginPath();
+    // ctx.arc(
+    //   this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true
+    // );
+    // ctx.fill();
+    ctx.drawImage(nemo, this.pos[0], this.pos[1])
 
   };
 
